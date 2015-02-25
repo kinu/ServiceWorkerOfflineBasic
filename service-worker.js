550a3399e33b17058d4f9e51ac1d6ad1a31a7ee6
Kinuko Yasuda <kinuko@chromium.org>
2015-02-25 14:56:36 +0900

    Updated for demo

--- service-worker.js
+++ service-worker.js
@@ -2,29 +2,43 @@ importScripts('serviceworker-cache-polyfill.js');
 
 self.oninstall = function(event) {
   event.waitUntil(
-    caches.open('statics-v1').then(function(cache) {
+    caches.open('statics-v3').then(function(cache) {
       return cache.addAll([
         '/',
         '/page.js',
+        '/nyancat.png',
         '/offline_icon.png',
         '/main.css']);
     })
   );
 };
 
+self.onactivate = function(event) {
+  event.waitUntil(caches.delete('statics-v2'));
+};
+
 self.onfetch = function(event) {
+  console.log('FETCHING:' + event.request.url);
+
+  // 画像だったら nyancat の画像にさしかえる
+  if (event.request.url.toLowerCase().indexOf('.png') != -1) {
+    event.respondWith(caches.match('/nyancat.png'));
+    return;
+  }
+
   /*
   // (1) キャッシュからだけ返す:
   event.respondWith(caches.match(event.request));
+  */
 
   // (2) キャッシュになかったらネットワークにfallback:
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
-    })
-  );
-  */
+          console.log('Fetched:', event.request, response);
+        }));
 
+  /*
   // (3) キャッシュになかったらネットワークにfallbackし、レスポンスを
   //     キャッシュする
   event.respondWith(
@@ -33,14 +47,14 @@ self.onfetch = function(event) {
         console.log(event.request.url, response);
         if (response)
           return response;
-        console.log('Fetching: ', event.request.url);
         fetch(event.request.clone()).then(function(response) {
           if (response.status < 400) {
+            console.log('Fetched: ', event.request, response);
             // HTTP response code がエラーじゃないときだけ fetch が返した
             // レスポンス(のコピー)をキャッシュする。
-            // (ただし、non-CORS リクエストについてはレスポンスは filtered
-            // opaque response となり、.status は常に 0 にセットされるため、
-            // エラーレスポンスをキャッシュしてしまう可能性はある
+            // (no-CORS リクエストについてはレスポンスは opaque filtered response となり、
+            // .status は常に 0 にセットされるため、エラーレスポンスをキャッシュしてしまう
+            // 可能性があります
             // https://fetch.spec.whatwg.org/#concept-filtered-response-opaque)
             cache.put(event.request, response.clone());
           }
@@ -49,4 +63,5 @@ self.onfetch = function(event) {
       })
     })
   );
+  */
 };
